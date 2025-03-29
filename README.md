62543d22db6d4c5c9e6dd2b0878d103818c5ae76
Hellothereobi123 <tharun.dilliraj@gmail.com>
2025-03-29 15:20:50 -0400

    samyan hello

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SAT_Hackpsu_2025
+
+Make sure you're in the same directory when installing. Install relevant modules using:
+
+"pip install -r requirements.txt
---
+++ api_req.py
@@ -0,0 +1,10 @@
+from google import genai
+
+client = genai.Client(api_key="AIzaSyBMSjV_Y7bmAq3DcDyG5S-B3Tcd5xcuMCE")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="Explain how AI works",
+)
+
+print(response.text)
